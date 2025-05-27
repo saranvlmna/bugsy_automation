5ba178b5a1978ddca9dffb8cddaec82fc1526e0b
Saranjith <saranvlmna@gmail.com>
2025-05-27 15:34:34 +0530

    feat: add browser user agent constant to shared constants

--- shared/constant.ts
+++ shared/constant.ts
@@ -1,3 +1,7 @@
 export const MODEL = {
   gpt_4o: "gpt-4o",
+  llama4: "llama-4-maverick-17b-128e-instruct",
 };
+
+export const BROWSER =
+  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36";

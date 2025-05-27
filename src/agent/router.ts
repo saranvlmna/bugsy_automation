413b69592bea7045f3a4ecd1d7bdfa7cfaf04e5d
Saranjith <saranvlmna@gmail.com>
2025-05-27 17:46:42 +0530

    feat: implement task details fetching endpoint and associated logic

--- src/agent/router.ts
+++ src/agent/router.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import balanceFetch from "./balance.fetch";
 import runtask from "./runtask";
+import fetchTaskDetails from "./fetch.task.details";
 
 const agentRouter = Router();
 
 agentRouter.get("/balance", balanceFetch);
 agentRouter.post("/task/run", runtask);
-
+agentRouter.get("/task/get", fetchTaskDetails);
 export default agentRouter;

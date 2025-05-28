e15715a6689e73b7bbd80680e6f0d371ff442cbe
Saranjith <saranvlmna@gmail.com>
2025-05-28 16:01:07 +0530

    feat: add CORS support and update dependencies
    - Added `cors` package to enable Cross-Origin Resource Sharing in the Express server.
    - Updated `@types/cors` to include TypeScript definitions for the new CORS package.
    - Cleaned up `yarn.lock` by removing unused dependencies and ensuring consistency.

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import "./config/env";
 
 import agentRouter from "./src/agent/router";
@@ -6,6 +7,8 @@ import systemRouter from "./src/system/router";
 
 const app = express();
 const port = process.env.APP_PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

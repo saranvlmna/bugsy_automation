e0986742f57296bd75fdfbf00bc74a6df4f4c2d4
Saranjith <saranvlmna@gmail.com>
2025-05-29 11:42:51 +0530

    refactor: adjust router to correct file upload handling for testcase run

--- src/system/router.ts
+++ src/system/router.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
+import multer, { memoryStorage } from "multer";
 import testcaseRun from "./testcase.run";
 import testcaseWrite from "./testcase.write";
-import multer, { memoryStorage } from "multer";
 const upload = multer({ storage: memoryStorage() });
 
 const systemRouter = Router();
 
 systemRouter.post("/testcase/write", testcaseWrite);
-systemRouter.post("/testcase/run", upload.single("file"), testcaseRun);
+systemRouter.post("/testcase/run", testcaseRun);
+systemRouter.post("/testcase/file/run", upload.single("file"), testcaseRun);
 
 export default systemRouter;

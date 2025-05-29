e5c29c1cc917b0d7215cba36f29eb84be438c762
Saranjith <saranvlmna@gmail.com>
2025-05-29 11:43:23 +0530

    refactor: streamline Excel file creation and enhance test case parsing logic

--- src/system/lib/excel.file.create.ts
+++ src/system/lib/excel.file.create.ts
@@ -1,41 +1,21 @@
-import * as xlsx from 'xlsx';
-import { TestCase } from '../interfase/testcase';
+import * as xlsx from "xlsx";
 
 export default async (data: any) => {
-    try {
-        const parsed = transformToExcelFormat(data)
+  try {
+    const workbook = xlsx.utils.book_new();
 
-        const workbook = xlsx.utils.book_new();
+    const sheet = xlsx.utils.json_to_sheet(data);
 
-        const sheet = xlsx.utils.json_to_sheet(parsed);
+    xlsx.utils.book_append_sheet(workbook, sheet, "Sheet1");
 
-        xlsx.utils.book_append_sheet(workbook, sheet, 'Sheet1');
+    const excelBuffer = xlsx.write(workbook, {
+      bookType: "xlsx",
+      type: "buffer",
+    });
 
-        const excelBuffer = xlsx.write(workbook, {
-            bookType: "xlsx",
-            type: "buffer"
-        });
-
-        return excelBuffer;
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-};
-
-const transformToExcelFormat = (testCases: TestCase[]) => {
-  return testCases.map((item, index) => ({
-    'Sl. No': index + 1,
-    'Test Case ID': `TSC-${index + 1}`,
-    'Req. ID/User Story': '',
-    'Priority': '',
-    'Scenario/Test Case Description': item.description,
-    'Pre-Condition': item.pre_condition,
-    'Test Steps': item.test_steps.join('\n'),
-    'Test Data': '',
-    'Expected Result': item.expected_result,
-    'Actual Result': '',
-    'Status': ''
-  }));
+    return excelBuffer;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
--- src/system/lib/testcase.parse.ts
+++ src/system/lib/testcase.parse.ts
@@ -4,14 +4,31 @@ export default (text: string | null): TestCase[] => {
   try {
     if (!text) return [];
 
-    const json = text
+    let json = text
       .replace(/^```json\s*/, "")
       .replace(/```$/, "")
       .trim();
-
-    return JSON.parse(json);
+    json = JSON.parse(json);
+    const transformedData = toDataTransform(json);
+    return transformedData;
   } catch (err) {
     console.error(err);
     throw err;
   }
+};
+
+const toDataTransform = (testCases: any) => {
+  return testCases.map((item: any, index: any) => ({
+    "Sl. No": index + 1,
+    "Test Case ID": `TSC-${index + 1}`,
+    "Req. ID/User Story": "",
+    Priority: "",
+    "Scenario/Test Case Description": item.description,
+    "Pre-Condition": item.pre_condition,
+    "Test Steps": item.test_steps.join("\n"),
+    "Test Data": "",
+    "Expected Result": item.expected_result,
+    "Actual Result": "",
+    Status: "",
+  }));
 };
